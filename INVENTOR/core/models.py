7c2aa7bf29a94b0e548de2a045a588ee7b558cac
KDivalien <k.divalien@gmail.com>
2023-08-18 16:14:47 +0200

    model suite

--- INVENTOR/core/models.py
+++ INVENTOR/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+CATEGORY=(
+    ('Stationary','stationary'),
+    ('Electronics','Electronics'),
+    ('Food','food')
+)
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    category=models.CharField(max_length=100, choices=CATEGORY)
+    quantity=models.PositiveIntegerField(null=True)
+
+    class Meta:
+        verbose_name_plural ='Product'
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
+
+
+class Order(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    staff=models.ForeignKey(User,models.CASCADE,null=True)
+    order_quantity=models.PositiveBigIntegerField(null=True)
+    date=models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name_plural ='Order'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
